6577a615452f37cd7d3070033f408eb8aade4824
Zamin Zafar <zamin.zafar72@gmail.com>
2026-08-11 02:07:36 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 // Swagger Documentation
